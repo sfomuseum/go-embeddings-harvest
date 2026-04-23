b2fa1f2ff3164a512e845f9fa9f6cd655efac713
thisisaaronland <thisisaaronland@localhost>
2026-04-23 12:54:40 -0700

    harvest MoMA

---
+++ cmd/harvest-moma-embeddings/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/sfomuseum/go-csvdict/v2"
+	sfom_embeddings "github.com/sfomuseum/go-embeddings"
+	"github.com/sfomuseum/go-embeddings-harvest"
+	"github.com/sfomuseum/go-embeddingsdb/parquet"
+	"github.com/sfomuseum/go-flags/flagset"
+	"github.com/sfomuseum/go-flags/multi"
+)
+
+func main() {
+
+	var embeddings_client_uri string
+
+	var artworks string
+
+	var workers int
+	var output string
+	var verbose bool
+	var models multi.MultiCSVString
+
+	fs := flagset.NewFlagSet("nga")
+
+	fs.StringVar(&artworks, "artworks", "", "The path to the 'Artworks.csv' file contained in the MuseumofModernArt/collection GitHub repository.")
+
+	fs.IntVar(&workers, "workers", 5, "The number of workers to use to fetch images (and derive embeddings) concurrently")
+	fs.Var(&models, "model", "One or more models to derive embeddings for. This may also be a comma-separated list.")
+
+	fs.StringVar(&output, "output", "-", "The path where Parquet-encoded data should be written. If \"-\" then data will be written to STDOUT.")
+	fs.StringVar(&embeddings_client_uri, "embeddings-client-uri", "mobileclip://?client-uri=grpc://localhost:8080", "A registered sfomuseum/go-embeddingsdb/client.Client URI.")
+	fs.BoolVar(&verbose, "verbose", false, "Enable verbose (debug) logging.")
+
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Generate Parquet-encoded embeddings from the Museum of Modern Art (MoMA) open data release.\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n\t%s [options]", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Valid options are:\n")
+		fs.PrintDefaults()
+	}
+
+	flagset.Parse(fs)
+
+	if verbose {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+		slog.Debug("Verbose logging enabled")
+	}
+
+	ctx := context.Background()
+
+	if len(models) == 0 {
+		log.Fatal("No models defined")
+	}
+
+	emb_cl, err := sfom_embeddings.NewEmbedder32(ctx, embeddings_client_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to create embeddings client, %v", err)
+	}
+
+	wr, err := parquet.NewWriter(ctx, output)
+
+	if err != nil {
+		log.Fatalf("Failed to create new writer, %v", err)
+	}
+
+	artworks_r, err := csvdict.NewReaderFromPath(artworks)
+
+	if err != nil {
+		log.Fatalf("Failed to create CSV reader for artworks, %v", err)
+	}
+
+	count := int64(0)
+	done_ch := make(chan bool)
+
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-done_ch:
+				return
+			case <-ticker.C:
+				slog.Info("Processed rows", "count", count)
+			}
+		}
+	}()
+
+	throttle := make(chan bool, workers)
+
+	for i := 0; i < workers; i++ {
+		throttle <- true
+	}
+
+	wg := new(sync.WaitGroup)
+
+	for row, err := range artworks_r.Iterate() {
+
+		if err != nil {
+			log.Fatalf("Artworks iterator yielded an error, %v", err)
+		}
+
+		<-throttle
+
+		wg.Go(func() {
+
+			defer func() {
+				throttle <- true
+			}()
+
+			count += 1
+
+			depiction_id := ""
+			subject_id := row["ObjectID"]
+			im_url := row["ImageURL"]
+
+			logger := slog.Default()
+			logger = logger.With("subject", subject_id)
+
+			if im_url == "" {
+				logger.Error("No image URL")
+				return
+			}
+
+			im_u, err := url.Parse(im_url)
+
+			if err != nil {
+				logger.Error("Failed to parse image URL", "url", im_url, "error", err)
+				return
+			}
+
+			im_q := im_u.Query()
+
+			if !im_q.Has("sha") {
+				logger.Error("Image URL missing ?sha", "url", im_url)
+				return
+			}
+
+			depiction_id = im_q.Get("sha")
+
+			logger.Debug("Fetch image", "url", im_url)
+
+			im_rsp, err := http.Get(im_url)
+
+			if err != nil {
+				logger.Error("Failed to retrieve image", "url", im_url, "error", err)
+				return
+			}
+
+			im_body, err := io.ReadAll(im_rsp.Body)
+			im_rsp.Body.Close()
+
+			if err != nil {
+				logger.Error("Failed to read image", "url", im_url, "error", err)
+				return
+			}
+
+			attrs := map[string]string{
+				"type":               "image",
+				"preview":            im_url,
+				"subject_url":        row["URL"],
+				"subject_title":      row["Title"],
+				"subject_creditline": row["CreditLine"],
+				"provider_name":      "Museum of Modern Art",
+				"provider_url":       "https://www.moma.org/",
+			}
+
+			derive_opts := &harvest.DeriveEmbeddingsRecordsOptions{
+				Provider:    "moma",
+				DepictionId: depiction_id,
+				SubjectId:   subject_id,
+				Attributes:  attrs,
+				Models:      models,
+				Body:        im_body,
+			}
+
+			records, err := harvest.DeriveEmbeddingsRecords(ctx, emb_cl, derive_opts)
+
+			if err != nil {
+				logger.Error("Failed to derive embeddings records", "error", err)
+				return
+			}
+
+			if len(records) == 0 {
+				logger.Warn("Failed to derive embeddings")
+				return
+			}
+
+			_, err = wr.Write(records)
+
+			if err != nil {
+				logger.Error("Failed to write records buffer", "url", im_url, "error", err)
+				return
+			}
+
+			logger.Debug("Wrote embeddings for exhibition image", "url", im_url)
+		})
+
+		wr.Flush()
+	}
+
+	wg.Wait()
+
+	err = wr.Close()
+
+	if err != nil {
+		log.Fatalf("Failed to close after writing, %v", err)
+	}
+
+}
