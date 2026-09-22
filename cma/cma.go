6efd89d14fcbd1b6f3533eafa1066191a9d1ec0f
thisisaaronland <thisisaaronland@localhost>
2026-09-22 10:03:03 -0700

    type generic wah wah...

---
+++ cma/cma.go
@@ -0,0 +1,5 @@
+package cma
+
+// https://www.clevelandart.org/open-access
+// https://openaccess-api.clevelandart.org/#fields
+// https://github.com/ClevelandMuseumArt/openaccess
--- harvester.go
+++ harvester.go
@@ -6,10 +6,12 @@ import (
 	"iter"
 	"context"
 
+	"github.com/sfomuseum/go-blobcache/http"	
+	"github.com/sfomuseum/go-embeddings"	
 	"github.com/sfomuseum/go-embeddingsdb"
 )
 
 type Harvester interface {
-	Iterate(context.Context) iter.Seq2[[]*embeddingsdb.Record, error]
+	Iterate(context.Context, embeddings.Embedder, *http.GetWithCacheOptions) iter.Seq2[[]*embeddingsdb.Record, error]
 	Close() error
 }
